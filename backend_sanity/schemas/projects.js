2fbbf634a9b5eebf39ad4f6828ba0159d55b38ad
duyendtcidm <duyendtcidm@gmail.com>
2022-04-24 09:23:43 +0700

    backend 1st

---
+++ backend_sanity/schemas/projects.js
@@ -0,0 +1,24 @@
+export default {
+    name: 'projects',
+    title: 'Projects',
+    type: 'document',
+    fields: [{
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'string'
+        },
+        {
+            name: 'imgUrl',
+            title: 'ImgUrl',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        },
+    ]
+}
---
+++ backend_sanity/schemas/testimonials.js
@@ -0,0 +1,29 @@
+export default {
+    name: 'testimonials',
+    title: 'Testimonials',
+    type: 'document',
+    fields: [{
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+        {
+            name: 'university',
+            title: 'University',
+            type: 'string'
+        },
+        {
+            name: 'imageurl',
+            title: 'ImgURL',
+            type: 'image',
+            options: {
+                hotspot: true,
+            }
+        },
+        {
+            name: 'feedback',
+            title: 'Feedback',
+            type: 'string'
+        },
+    ],
+}
